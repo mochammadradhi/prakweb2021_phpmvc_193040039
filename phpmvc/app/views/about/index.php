79b33d184ca4b2bf4a565728cc04f9981df1af52
mochammadradhi <m.radhiakbar@gmail.com>
2021-09-30 14:12:37 +0700

    Menyelesaikan Tugas Aplikasi MVC dengan PHP Video 6 mengenai Asset

--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,3 +1,15 @@
 
-    <h1>About me</h1>
-    <p>Hallo, nama saya <?= $data['nama'] ?> pekerjaan saya <?= $data['pekerjaan'] ?>. dan saya berumur <?= $data['umur'] ?> tahun.</p>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-7">
+            <h2 class="display-1">About me</h2>
+            <p class="lead">Hallo, nama saya <?= $data['nama'] ?> pekerjaan saya <?= $data['pekerjaan'] ?>. dan saya berumur <?= $data['umur'] ?> tahun.</p>
+        
+        </div>
+        <div class="col-md-5">
+        <img src="<?= BASEURL; ?>img/meong9gag.jpg" class="card-img-top" alt="...">
+        </div>
+    </div>
+    <hr class="my-4">
+</div>
+
--- phpmvc/app/views/home/index.php
+++ phpmvc/app/views/home/index.php
@@ -1,2 +1,20 @@
 
-    <h1>Welcome! to my Website!</h1>
+
+
+
+<div class="container mt-4">
+<div class="jumbotron">
+<figure class="text-center">
+  <blockquote class="blockquote">
+    <h3>Hello. Welcome to my Website</h3>
+  </blockquote>
+  <figcaption class="blockquote-footer">
+    Radhi <cite title="Source Title">Student Pasundan University</cite>
+  </figcaption>
+</figure> 
+<hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+
+</div>
